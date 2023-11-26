d6f5afb2a870586cd20cf8f10f8d590a829d324c
Oleg Mirasov <oleg.mirasov@mail.ru>
2023-11-26 13:28:41 +0300

    some added to app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from use_tools.main_loop import MainLoop
 
 
 class App:
@@ -9,15 +10,32 @@ class App:
         self.h = h
         self.screen = pg.display.set_mode((self.w, self.h))
 
-        self.clock = pg.time.Clock()
-        self._running = True
+        self.main_loop = MainLoop(self.update, self.draw)
+        self.__updates = []
 
     def run(self):
-        while self._running:
-            pass
+        self.main_loop.run()
+
+    def update(self, events):
+        for event in events:
+            if event.type == pg.QUIT:
+                self.exit()
+
+        for i in self.__updates:
+            i()
+
+    def draw(self):
+        pass
 
     def exit(self):
-        pass
+        self.main_loop.exit()
+
+    def add_update_function(self, func):
+        self.__updates.append(func)
+
+    def remove_update_function(self, func):
+        if func in self.__updates:
+            self.__updates.remove(func)
 
 
 if __name__ == '__main__':
--- use_tools/main_loop.py
+++ use_tools/main_loop.py
@@ -2,5 +2,26 @@ import pygame as pg
 
 
 class MainLoop:
-    def __init__(self, uf=None, df=None):
-        pass
+    def __init__(self, uf=None, df=None, fps=60):
+        """
+        uf - update function, take 1 argument - events;
+        df - draw function, no argument;
+        fps - FPS (count cycles per second.
+        """
+        self._update = uf if uf else lambda x: None
+        self._draw = df if df else lambda: None
+        self._fps = fps
+
+        self._clock = pg.time.Clock()
+        self.__running = True
+
+    def run(self):
+        while self.__running:
+            events = pg.event.get()
+            self._update(events)
+            self._draw()
+
+            self._clock.tick(self._fps)
+
+    def exit(self):
+        self.__running = False
